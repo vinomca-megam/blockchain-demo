b24b2e1eaf4b53234abe7dc902b86f50cd6c89d5
vinomca-megam <vino.v@megam.io>
2018-06-07 17:55:27 +0530

    file deleted

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "******** build start ******"
-
-cd /tmp/src
-pwd
-npm install
-
-echo "******** build end ******"
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "***************************"
-echo "    Starting..."
-echo "***************************"
-cd /tmp/src/
-pwd
-npm start
